91eb257469a5ee2aa8fefcef90a0fbe52eaad414
taylorchu <tailinchu@gmail.com>
2015-08-22 09:47:56 -0700

    use lpm matcher in loop checker

--- loop_checker.go
+++ loop_checker.go
@@ -2,51 +2,31 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 
+	"github.com/go-ndn/lpm"
 	"github.com/go-ndn/mux"
 	"github.com/go-ndn/ndn"
 )
 
 func loopChecker(next mux.Handler) mux.Handler {
-	m := newMatcher()
+	m := lpm.NewThreadSafe()
 	return mux.HandlerFunc(func(w ndn.Sender, i *ndn.Interest) {
 		interestID := fmt.Sprintf("%s/%x", i.Name, i.Nonce)
-		if m.Match(interestID) {
+		var ok bool
+		m.Update(interestID, func(v interface{}) interface{} {
+			ok = v == nil
+			return struct{}{}
+		}, false)
+		if !ok {
 			return
 		}
 		go func() {
 			time.Sleep(time.Minute)
-			m.Remove(interestID)
+			m.Update(interestID, func(interface{}) interface{} {
+				return nil
+			}, false)
 		}()
 		next.ServeNDN(w, i)
 	})
-}
-
-type matcher struct {
-	m map[string]struct{}
-	sync.Mutex
-}
-
-func newMatcher() *matcher {
-	return &matcher{
-		m: make(map[string]struct{}),
-	}
-}
-
-func (m *matcher) Remove(s string) {
-	m.Lock()
-	delete(m.m, s)
-	m.Unlock()
-}
-
-func (m *matcher) Match(s string) bool {
-	m.Lock()
-	defer m.Unlock()
-	_, ok := m.m[s]
-	if !ok {
-		m.m[s] = struct{}{}
-	}
-	return ok
 }
